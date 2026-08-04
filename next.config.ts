f95d32e8fd6a8874006df793c1734143e2c64d93
TaH00R <bgmiseason002@gmail.com>
2026-08-04 23:35:16 +0530

    Added Deployment Actions

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,11 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-
   images: {
     unoptimized: true,
   },
-
   basePath: isProd ? "/portfolio" : "",
   assetPrefix: isProd ? "/portfolio/" : "",
-
-  trailingSlash: true,
 };
 
 export default nextConfig;
